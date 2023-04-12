445f66d8c1d61cf59c7a4821aea03cdfaebd24f4
Franco Ponticelli <franco.ponticelli@gmail.com>
2023-04-11 21:48:44 -0600

    Animate rolling

--- src/components/RollView.tsx
+++ src/components/RollView.tsx
@@ -1,4 +1,4 @@
-import { type JSX, type Signal, When } from '@tempots/dom'
+import { type JSX, type Signal, When, Prop } from '@tempots/dom'
 import { type State } from '../state'
 import { Action } from '../action'
 import { RR } from 'dicerollerts'
@@ -9,10 +9,55 @@ export interface RollViewProps {
   state: Signal<State>
 }
 
+function lerpf (a: number, b: number, t: number): number {
+  return a + (b - a) * t
+}
+
+function animateSignal<T> (
+  signal: Signal<T>,
+  interpolate: (start: T, end: T, t: number) => T,
+  initial: T | undefined = undefined,
+  duration: number = 200
+): Signal<T> {
+  const prop = Prop.of(initial ?? signal.get())
+  signal.subscribe(v => {
+    const start = prop.get()
+    const end = v
+    const startAt = Date.now()
+    const endAt = startAt + duration
+    const step = (): void => {
+      const now = Date.now()
+      if (now >= endAt) {
+        prop.set(end)
+      } else {
+        prop.set(interpolate(start, end, (now - startAt) / duration))
+        requestAnimationFrame(step)
+      }
+    }
+    requestAnimationFrame(step)
+  })
+  return prop
+}
+
+function animateNumberSignal (signal: Signal<number>, initial: number | undefined = undefined, duration: number = 200): Signal<number> {
+  return animateSignal(signal, lerpf, initial, duration)
+}
+
+function animateIntSignal (signal: Signal<number>, initial: number | undefined = undefined, duration: number = 200): Signal<number> {
+  return animateSignal(signal, (a, b, t) => Math.round(lerpf(a, b, t)), initial, duration)
+}
+
 export function RollView ({ dispatch, state }: RollViewProps): JSX.DOMNode {
   const updateSeed = (seed: number): void => { dispatch(Action.updateSeed(seed)) }
   const toggleSeed = (): void => { dispatch(Action.toggleUseSeed()) }
   const roll = (): void => { dispatch(Action.roll()) }
+  const result = animateIntSignal(state.at('roll').map(v => {
+    if (v == null) {
+      return 0
+    } else {
+      return RR.getResult(v)
+    }
+  }))
   return (
     <div class="roll-box">
       <div class="rolling">
@@ -24,13 +69,7 @@ export function RollView ({ dispatch, state }: RollViewProps): JSX.DOMNode {
             }}
             href="#"
           >
-            {state.at('roll').map(v => {
-              if (v == null) {
-                return 'roll'
-              } else {
-                return RR.getResult(v)
-              }
-            })}
+            {result}
           </a>
         </div>
         <SeedControls useSeed={state.at('useSeed')} seed={state.at('seed')} updateSeed={updateSeed} toggleSeed={toggleSeed} />
